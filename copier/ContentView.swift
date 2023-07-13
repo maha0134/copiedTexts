7a4401e9f44e5302922de218b915cc478e720d17
Akshay Mahajan <maha0134@algonquinlive.com>
2023-07-13 15:39:23 +0530

    add basic layout to copy text using a textfield

--- copier/ContentView.swift
+++ copier/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+	
+	@State var copiedStrings = [String]()
+	@State var text: String = ""
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+			Text("Copied Texts")
+				.font(.title)
+				.padding(15)
+			Text("This app listens to changes to the clipboard")
+			TextField(text: $text) {
+				Text("Type text here to copy")
+			}
+			.autocorrectionDisabled()
+			.padding(7.5)
+			.frame(width: 200)
+			.border(.black)
+			Text("Copied texts show up here")
+				.padding()
+			if !copiedStrings.isEmpty {
+				List(copiedStrings, id: \.self) { copiedString in
+					Text(copiedString)
+				}
+			} else {
+				Text("No copied text added to the clipboard")
+					.foregroundColor(.red)
+			}
         }
         .padding()
+		.onAppear {
+			NotificationCenter.default.addObserver(forName: UIPasteboard.changedNotification, object: nil, queue: nil) { _ in
+				updateClipboard()
+			}
+		}
     }
 }
 
@@ -23,4 +50,16 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+extension ContentView {
+	func updateClipboard() {
+		if let copiedText = UIPasteboard.general.string {
+			if !copiedStrings.contains(copiedText) {
+				copiedStrings.append(copiedText)
+			}
+			
+		}
+	}
 }
