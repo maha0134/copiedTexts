6f1910529798e65a1d9e774100bc8f504567dd5b
Akshay Mahajan <maha0134@algonquinlive.com>
2023-07-13 13:56:17 +0530

    Initial Commit

---
+++ copier/copierApp.swift
@@ -0,0 +1,17 @@
+//
+//  copierApp.swift
+//  copier
+//
+//  Created by AKSHAY MAHAJAN on 2023-07-13.
+//
+
+import SwiftUI
+
+@main
+struct copierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
